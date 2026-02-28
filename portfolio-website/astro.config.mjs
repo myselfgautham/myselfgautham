6c70fbdafa48776b52035831231d17fcce5484fc
Gautham Krishna Vinayachandran <vinayachandran@aol.com>
2026-02-28 14:40:09 +0530

    Fixed Path Errors II

--- portfolio-website/astro.config.mjs
+++ portfolio-website/astro.config.mjs
@@ -5,4 +5,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
     site: "https://myselfgautham.github.io",
     base: "/myselfgautham",
+    trailingSlash: "always",
 });
